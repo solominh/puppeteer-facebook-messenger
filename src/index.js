7623e8ceb765de598dfe160b7f0a6553a33b7263
Minh Tran <hoangminhftu2@gmail.com>
2018-12-24 16:09:55 +0700

    [Bot] get Text Message from UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,61 @@
-const puppeteer = require('puppeteer')
-const { loginFacebook } = require('./loginFacebook')
+const puppeteer = require('puppeteer');
+const { loginFacebook } = require('./loginFacebook');
+
+const selectors = {
+  allMessagesSelector:
+    '.fbNubFlyoutOuter  .fbNubFlyoutInner  ._1i6a  ._4po5  ._4po8  ._4po9  div._4tdt',
+  lastMessageSelector:
+    '.fbNubFlyoutOuter  .fbNubFlyoutInner  ._1i6a  ._4po5  ._4po8  ._4po9  div._4tdt:last-child',
+  textMessagesSelector: 'div._4gx_ div._1aa6 span._5yl5',
+};
+
+const openChatWindow = async page => {
+  await page.$$eval('button[type="submit"]', buttons => {
+    const results = Array.from(buttons).filter(
+      b => b.textContent === 'Send Message'
+    );
+    results[0].click();
+  });
+
+  await page.waitForSelector(selectors.allMessagesSelector);
+};
+
+const sendMessage = async (page, message) => {
+  await openChatWindow(page);
+  await page.keyboard.type(message);
+  await page.keyboard.press('Enter');
+};
+
+const getBotReplyMessage = async page => {
+  const jsHandle = await page.evaluateHandle(selectors => {
+    const lastMessage = document.querySelector(selectors.lastMessageSelector);
+    const isBotMessage = lastMessage.className.contains('_ua1');
+    console.log(lastMessage);
+    if (isBotMessage) return lastMessage;
+    return null;
+  }, selectors);
+
+  return jsHandle.asElement();
+};
+
+const getBotReplyTextMessages = async page => {
+  while (true) {
+    await page.waitFor(1000);
+    const botMessage = await getBotReplyMessage(page);
+    if (!botMessage) continue;
+
+    const textMessages = await botMessage.$$eval(
+      selectors.textMessagesSelector,
+      els =>
+        els.map(el => {
+          while (el.nodeType !== 3) el = el.firstChild;
+          return el.textContent;
+        })
+    );
+
+    return textMessages;
+  }
+};
 
 const main = async () => {
   const browser = await puppeteer.launch({
@@ -10,17 +66,33 @@ const main = async () => {
       '--disable-setuid-sandbox',
       '--disable-notifications',
     ],
-  })
+  });
 
-  let page = await browser.newPage()
+  let page = await browser.newPage();
   await page.setViewport({
-    width: 1200,
-    height: 900,
-  })
+    width: 1000,
+    height: 700,
+  });
 
-  await loginFacebook(page)
+  await loginFacebook(page);
 
-  console.log('Done.')
-}
+  const url =
+    'https://www.facebook.com/BoCobot-Dev-529334300818600/?modal=admin_todo_tour';
+  await page.goto(url);
 
-main()
+  await sendMessage(page, 'hello');
+  let textMessages = await getBotReplyTextMessages(page);
+  console.log(textMessages);
+
+  await sendMessage(page, 'hello again');
+  textMessages = await getBotReplyTextMessages(page);
+  console.log(textMessages);
+
+  await sendMessage(page, 'hello again again');
+  textMessages = await getBotReplyTextMessages(page);
+  console.log(textMessages);
+
+  console.log('Done.');
+};
+
+main();
