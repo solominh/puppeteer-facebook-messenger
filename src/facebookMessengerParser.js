58e098192d5c69c67a9438aec7588740a330c71e
Minh Tran <hoangminhftu2@gmail.com>
2018-12-25 09:04:19 +0700

    Add sendMessageButtonSelector

--- src/facebookMessengerParser.js
+++ src/facebookMessengerParser.js
@@ -1,19 +1,21 @@
 const selectors = {
   allMessagesSelector:
     '.fbNubFlyoutOuter  .fbNubFlyoutInner  ._1i6a  ._4po5  ._4po8  ._4po9  div._4tdt',
+  userMessagesSelector:
+    '.fbNubFlyoutOuter  .fbNubFlyoutInner  ._1i6a  ._4po5  ._4po8  ._4po9  div._4tdt._ua0',
+  botMessagesSelector:
+    '.fbNubFlyoutOuter  .fbNubFlyoutInner  ._1i6a  ._4po5  ._4po8  ._4po9  div._4tdt._ua1',
   lastMessageSelector:
     '.fbNubFlyoutOuter  .fbNubFlyoutInner  ._1i6a  ._4po5  ._4po8  ._4po9  div._4tdt:last-child',
   textMessagesSelector: 'div._4gx_ div._1aa6 span._5yl5',
+  sendMessageButtonSelector: "._4bl9 button[type='submit']",
 };
 
 const openChatWindow = async page => {
-  await page.$$eval('button[type="submit"]', buttons => {
-    const results = Array.from(buttons).filter(
-      b => b.textContent === 'Send Message'
-    );
-    results[0].click();
-  });
-
+  const sendMessageButton = await page.$(
+    selectors.sendMessageButtonSelector
+  );
+  await sendMessageButton.click();
   await page.waitForSelector(selectors.allMessagesSelector);
 };
 
@@ -23,12 +25,13 @@ const sendMessage = async (page, message) => {
   await page.keyboard.press('Enter');
 };
 
-const getBotReplyMessage = async page => {
+const getBotReplyMessageElementHandle = async page => {
   const jsHandle = await page.evaluateHandle(selectors => {
     const lastMessage = document.querySelector(selectors.lastMessageSelector);
     const isBotMessage = lastMessage.className.contains('_ua1');
     console.log(lastMessage);
     if (isBotMessage) return lastMessage;
+
     return null;
   }, selectors);
 
@@ -38,17 +41,19 @@ const getBotReplyMessage = async page => {
 const getBotReplyTextMessages = async page => {
   while (true) {
     await page.waitFor(1000);
-    const botMessage = await getBotReplyMessage(page);
-    if (!botMessage) continue;
+    const botMessageElementHandle = await getBotReplyMessageElementHandle(page);
+    if (!botMessageElementHandle) continue;
 
-    const textMessages = await botMessage.$$eval(
-      selectors.textMessagesSelector,
-      els =>
+    const textMessages = await botMessageElementHandle
+      .asElement()
+      .$$eval(selectors.textMessagesSelector, els =>
         els.map(el => {
           while (el.nodeType !== 3) el = el.firstChild;
           return el.parentElement.textContent;
         })
-    );
+      );
+
+    await botMessageElementHandle.dispose();
 
     return textMessages;
   }
